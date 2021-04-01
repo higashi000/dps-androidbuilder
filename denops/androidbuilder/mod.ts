9a0d7240d03b67b45327eebf8c1f3eed99881a45
higashi000 <higashi.ict@gmail.com>
2021-04-02 01:08:38 +0900

    add: execute install debug

--- denops/androidbuilder/mod.ts
+++ denops/androidbuilder/mod.ts
@@ -3,20 +3,34 @@ import SearchGradlew from "./search_gradlew.ts";
 
 start(async (vim) => {
     vim.register({
-        async AssembleDebug(): Promise<void> {
-            const path = await SearchGradlew();
+        async install_debug(): Promise<void> {
+            const nowDir = await vim.call('getcwd') as string;
+
+            const path = await SearchGradlew(nowDir + "/");
 
             if (path === "error: `Gradlew` is not found in current directory") {
                 console.log("da;slkfja;klgjarg;lrkjg");
                 return;
             }
 
-            console.log("hogehoge");
-        }
-    })
+
+            console.log("a");
+            await vim.cmd('cd ' + path);
+
+            await vim.cmd('!./gradlew installDebug')
+
+            await vim.cmd('cd ' + nowDir);
+
+        },
+
+        async hello(): Promise<void> {
+            console.log("hogehogejfa;ldkjas;dkljbva");
+        },
+    });
 
     vim.execute(`
-        command! AndroidBuilder :call denops#request("androidbuilder", "AssembleDebug", [])
+        command! AndroidBuilder :call denops#request("androidbuilder", "install_debug", [])
+        command! AndroidBuilderHello :call denops#request("androidbuilder", "hello", [])
     `)
 
     console.log("dps-androidbuilder has loaded!");
