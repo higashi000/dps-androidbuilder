77ebe0fbf81e2beda8436e169f0caf0c5a520d19
higashi000 <higashi.ict@gmail.com>
2021-04-01 17:50:59 +0900

    add: search of Gradlew in parent directory

--- denops/androidbuilder/mod.ts
+++ denops/androidbuilder/mod.ts
@@ -1,5 +1,23 @@
 import { start } from "https://deno.land/x/denops_std@v0.2/mod.ts";
+import SearchGradlew from "./search_gradlew.ts";
 
 start(async (vim) => {
+    vim.register({
+        async AssembleDebug(): Promise<void> {
+            const path = await SearchGradlew();
+
+            if (path === "error: `Gradlew` is not found in current directory") {
+                console.log("da;slkfja;klgjarg;lrkjg");
+                return;
+            }
+
+            console.log("hogehoge");
+        }
+    })
+
+    vim.execute(`
+        command! AndroidBuilder :call denops#request("androidbuilder", "AssembleDebug", [])
+    `)
+
     console.log("dps-androidbuilder has loaded!");
 })
--- denops/androidbuilder/search_gradlew.ts
+++ denops/androidbuilder/search_gradlew.ts
@@ -1,5 +1,5 @@
-async function GetCurrentDirFiles(): Promise<boolean> {
-    for await (const dirEntry of Deno.readDir('./')) {
+async function ExistGradlew(filepath: string): Promise<boolean> {
+    for await (const dirEntry of Deno.readDir(filepath)) {
         if (dirEntry.name === "Gradlew") {
             return true;
         }
@@ -8,4 +8,33 @@ async function GetCurrentDirFiles(): Promise<boolean> {
     return false;
 }
 
-console.log(await GetCurrentDirFiles());
+async function SearchGradlew(): Promise<string> {
+    let path = "./";
+
+
+    let existGradlew: boolean = false;
+
+    let currentDir = new URL(path, import.meta.url).pathname;
+    while (true) {
+
+        if (await ExistGradlew(currentDir)) {
+            existGradlew = true;
+            break;
+        }
+
+        if (currentDir === "/home/") {
+            break;
+        }
+
+        path = path + "../";
+        currentDir = new URL(path, import.meta.url).pathname;
+    }
+
+    if (existGradlew) {
+        return currentDir;
+    } else {
+        return "error: `Gradlew` is not found in current directory";
+    }
+}
+
+export default SearchGradlew;
